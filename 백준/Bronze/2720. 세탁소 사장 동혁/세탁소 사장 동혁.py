bd47668eb0fe2eab5fbcc73e3c168f25d279e6e1
Gyeom Park <dotori634@gmail.com>
2023-07-14 20:01:34 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,12 @@
+change = [25, 10, 5, 1] # 쿼터, 다임, 니켈, 페니
+T = int(input())
+
+for _ in range(T):
+    C = int(input())
+    li = []
+
+    for i in change:
+        li.append(C // i) # 몫
+        C = C % i # 나머지 C에 할당
+
+    print(*li) 
